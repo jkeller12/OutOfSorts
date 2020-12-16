36b992c2db3139e590f227dfdb7ce358cea69a3e
Jonah Keller <jkeller10@stuy.edu>
2020-12-15 21:34:37 -0500

    i drafted up code real quick. If I hit this I'm god coder No compile yet

---
+++ Sorts.java
@@ -0,0 +1,33 @@
+public class Sorts{
+  /**Bubble sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+
+
+  public static void bubbleSort(int[] data)
+  { boolean InOrder = true;
+    while(InOrder == false)
+    {
+      int count = 0;
+      for (int i = 0; i < data.length; i++)
+      {
+        if (data[i] > data[i+1])
+        {
+          int Foward_Value = data[i];
+          data[i]= data[i+1];
+          data[i+1] = Foward_Value;
+        }
+        if(data[i] < data[i+1])
+        {
+          count ++;
+        }
+      }
+
+      if (count == data.length)
+      {
+        InOrder = true;
+      }
+    }
+  }
+}
